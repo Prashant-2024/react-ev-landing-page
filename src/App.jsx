be3cd57a7989b86771dc12a337b959fe0ec27712
Prashant Solanki <prashantsolanki2025@gmail.com>
2024-08-24 19:46:56 +0530

    Background Done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Background from "./components/Background/Background";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(0);
+
+  const [playStatus, setPlayStatus] = useState(false);
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
